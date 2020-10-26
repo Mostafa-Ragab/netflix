b66035d54726983a88d1d0c0d828edcd15714144
mostafa ragab <49085627+Mostafa-Ragab@users.noreply.github.com>
2020-10-26 15:19:41 +0200

    adding item styles

--- src/components/accordion/styles/accordion.js
+++ src/components/accordion/styles/accordion.js
@@ -31,10 +31,11 @@ export const Title = styled.h1`
 
 export const Item = styled.div`
 	color: white;
-	margin-bottom: 10px;
 	margin: auto;
 	max-width: 670px;
 	width: 100%;
+	margin-bottom: 10px;
+
 	&:first-of-type {
 		margin-top: 3em;
 	}
