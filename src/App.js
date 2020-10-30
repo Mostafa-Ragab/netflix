c82f490a9cd4515a0519440f0461cfa5d4d0faef
mostafa ragab <49085627+Mostafa-Ragab@users.noreply.github.com>
2020-10-30 09:20:30 +0200

    refactor the code

--- src/App.js
+++ src/App.js
@@ -5,6 +5,16 @@ import * as ROUTES from "./constants/routes";
 export function App() {
 	return (
 		<Router>
+			<Route exact path="/signin">
+				<SignIn />
+			</Route>
+			<Route exact path="/browse">
+				<Browse />
+			</Route>
+			<Route exact path="/signup">
+				<SignUp />
+			</Route>
+
 			<Route exact path={ROUTES.HOME}>
 				<Home />
 			</Route>
