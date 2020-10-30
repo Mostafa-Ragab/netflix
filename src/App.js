bf3909e718c21262ed0210c9b1d788adbb371702
mostafa ragab <49085627+Mostafa-Ragab@users.noreply.github.com>
2020-10-30 06:32:47 +0200

    adding Routes to App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from "react";
-import { JumbotronContainer } from "./containers/jumbotron";
-import { FooterContainer } from "./containers/footer";
-import { FaqsContainer } from "./containers/faqs";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Home, Browse, SignUp, SignIn } from "./pages";
+import * as ROUTES from "./constants/routes";
 export function App() {
 	return (
-		<>
-			<JumbotronContainer />
-			<FaqsContainer />
-			<FooterContainer />
-		</>
+		<Router>
+			<Route exact path={ROUTES.HOME}>
+				<Home />
+			</Route>
+		</Router>
 	);
 }
