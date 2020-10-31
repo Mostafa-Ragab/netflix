bf1aa1b634453df3e02e596ad6b6a59016973e54
mostafa ragab <49085627+Mostafa-Ragab@users.noreply.github.com>
2020-10-31 19:30:21 +0200

    adding sign up method

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { firebase } from "./lib/firebase.prod";
 import { FirebaseContext } from "./context/firebase";
 render(
 	<>
-		<FirebaseContext.Provider valu={{ firebase }}>
+		<FirebaseContext.Provider value={{ firebase }}>
 			<GlobalStyles />
 			<App />
 		</FirebaseContext.Provider>
