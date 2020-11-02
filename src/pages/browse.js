171db84114efdddb998868395483c3f4dec03675
mostafa ragab <49085627+Mostafa-Ragab@users.noreply.github.com>
2020-11-02 15:33:48 +0200

    adding browse page

--- src/pages/browse.js
+++ src/pages/browse.js
@@ -1,10 +1,17 @@
 import React from "react";
 import { useContent } from "../hooks";
+import selectFilter from "../utils/selection-filter";
+import BrowseContainer from "../containers/browse";
+
 export default function Browse() {
 	const { series } = useContent("series");
+	const { films } = useContent("films");
+
 	// I need the films and the series
 	// I need slides
+	const slides = selectFilter({ films, series });
+	console.log(slides);
 	// pass it to the browse container
 
-	return <p>I am from Browse!</p>;
+	return <BrowseContainer>{slides}</BrowseContainer>;
 }
