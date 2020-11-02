c26f210330b29f9bf130a02a6392183eb011aca5
mostafa ragab <49085627+Mostafa-Ragab@users.noreply.github.com>
2020-11-02 02:37:15 +0200

    adding is user redirect func to sign in and sign up

--- src/App.js
+++ src/App.js
@@ -1,20 +1,32 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import { Home, Browse, SignUp, SignIn } from "./pages";
 import * as ROUTES from "./constants/routes";
+import { IsUserRedirect } from "./helpers/routes";
+
 export function App() {
+	const user = {};
 	return (
 		<Router>
-			<Route exact path="/signin">
+			<IsUserRedirect
+				user={user}
+				loggedInPath={ROUTES.BROWSE}
+				path={ROUTES.SIGN_IN}
+				exact
+			>
 				<SignIn />
-			</Route>
+			</IsUserRedirect>
+			<IsUserRedirect
+				user={user}
+				loggedInPath={ROUTES.BROWSE}
+				path={ROUTES.SIGN_UP}
+				exact
+			>
+				<SignUp />
+			</IsUserRedirect>
 			<Route exact path="/browse">
 				<Browse />
 			</Route>
-			<Route exact path="/signup">
-				<SignUp />
-			</Route>
-
 			<Route exact path={ROUTES.HOME}>
 				<Home />
 			</Route>
