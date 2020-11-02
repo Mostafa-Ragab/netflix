e4a68f32418502f6f7a31207e50d32e35c8379f1
mostafa ragab <49085627+Mostafa-Ragab@users.noreply.github.com>
2020-11-02 06:53:11 +0200

    creating browse page

--- src/pages/browse.js
+++ src/pages/browse.js
@@ -1,5 +1,10 @@
 import React from "react";
-
+import { useContent } from "../hooks";
 export default function Browse() {
+	const { series } = useContent("series");
+	// I need the films and the series
+	// I need slides
+	// pass it to the browse container
+
 	return <p>I am from Browse!</p>;
 }
