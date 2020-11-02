ce34d6ee1cd14ba109c2316b5e763f4dbe951a0d
mostafa ragab <49085627+Mostafa-Ragab@users.noreply.github.com>
2020-11-02 15:31:36 +0200

    creating browse container

---
+++ src/containers/browse.js
@@ -0,0 +1,27 @@
+import React, { useState, useContext, useEffect } from "react";
+import { FirebaseContext } from "../context/firebase";
+import ProfileContainer from "./profile";
+import { Loading } from "../components";
+export default function BrowseContainer({ slides }) {
+	const [profile, setProfile] = useState({});
+
+	const [loading, setLoading] = useState(true);
+
+	const { firebase } = useContext(FirebaseContext);
+
+	const user = firebase.auth().currentUser || {};
+
+	useEffect(() => {
+		setTimeout(() => {
+			setLoading(false);
+		}, 3000);
+	}, [profile.displayName]);
+
+	return profile.displayName ? (
+		loading ? (
+			<Loading src={user.photoURL} />
+		) : null
+	) : (
+		<ProfileContainer user={user} setProfile={setProfile} />
+	);
+}
