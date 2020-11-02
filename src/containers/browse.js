f3a81dbbe4d227c02da587aec3bdb8089f21753f
mostafa ragab <49085627+Mostafa-Ragab@users.noreply.github.com>
2020-11-02 16:37:21 +0200

    adding feature

--- src/containers/browse.js
+++ src/containers/browse.js
@@ -20,8 +20,19 @@ export default function BrowseContainer({ slides }) {
 	return profile.displayName ? (
 		<>
 			{loading ? <Loading src={user.photoURL} /> : <Loading.ReleaseBody />}
+
 			<Header src="joker1">
-				<Header.Text>helloooooooo</Header.Text>
+				<Header.Feature>
+					<Header.FeatureCallOut>Watch Joker Now</Header.FeatureCallOut>
+					<Header.Text>
+						{" "}
+						Forever alone in a crowd, failed comedian Arthur Fleck seeks
+						connection as he walks the streets of Gotham City. Arthur wears two
+						masks -- the one he paints for his day job as a clown, and the guise
+						he projects in a futile attempt to feel like he's part of the world
+						around him.
+					</Header.Text>
+				</Header.Feature>
 			</Header>
 		</>
 	) : (
