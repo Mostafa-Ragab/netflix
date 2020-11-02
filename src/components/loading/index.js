cfe59c5472e6565e37cfaedc0eb32ac14fd57e83
mostafa ragab <49085627+Mostafa-Ragab@users.noreply.github.com>
2020-11-02 15:42:31 +0200

    spreding ...restProps in Spinner

--- src/components/loading/index.js
+++ src/components/loading/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Spinner, LockBody, Picture, RealesBody } from "./styles/loading";
 export default function Loading({ src, ...restProps }) {
 	return (
-		<Spinner>
+		<Spinner {...restProps}>
 			<LockBody />
 			<Picture src={`/images/users/${src}.png`} />
 		</Spinner>
