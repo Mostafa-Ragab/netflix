03cc90092e33f9aa2220bb8c2a14c1db71904c95
mostafa ragab <49085627+Mostafa-Ragab@users.noreply.github.com>
2020-11-02 15:41:26 +0200

    adding picture styles

--- src/components/loading/styles/loading.js
+++ src/components/loading/styles/loading.js
@@ -69,4 +69,12 @@ export const Spinner = styled.div`
 	}
 `;
 
-export const Picture = styled.img``;
+export const Picture = styled.img`
+	width: 50px;
+	height: 50px;
+	position: absolute;
+	top: 50%;
+	left: 50%;
+	margin-top: -100px;
+	margin-left: -22px;
+`;
