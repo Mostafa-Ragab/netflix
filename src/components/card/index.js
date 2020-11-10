ad5c461f7fd41ec457bdcd4f77832024c9bd201b
mostafa ragab <49085627+Mostafa-Ragab@users.noreply.github.com>
2020-11-10 08:41:16 +0200

    adding card functionalities

--- src/components/card/index.js
+++ src/components/card/index.js
@@ -1,1 +1,75 @@
-import React from "react";
+import React, { createContext, useState, useContext } from "react";
+import {
+	Item,
+	Group,
+	Title,
+	SubTitle,
+	Container,
+	Meta,
+	Text,
+	Image,
+	Entities,
+	Feature,
+	FeatureTitle,
+	FeatureClose,
+	FeatureText,
+	Maturity,
+	Content,
+} from "./styles/card";
+export const FeatureContext = createContext();
+
+export default function Card({ children, ...restProps }) {
+	const [showFeature, setShowFeature] = useState(false);
+	const [itemFeature, setItemFeature] = useState({});
+
+	return (
+		<FeatureContext.Provider
+			value={{ showFeature, setShowFeature, itemFeature, setItemFeature }}
+		>
+			<Container {...restProps}>{children}</Container>
+		</FeatureContext.Provider>
+	);
+}
+
+Card.Group = function CardGroup({ children, ...restProps }) {
+	return <Group {...restProps}>{children}</Group>;
+};
+
+Card.Title = function CardTitle({ children, ...restProps }) {
+	return <Title {...restProps}>{children}</Title>;
+};
+
+Card.SubTitle = function CardSubTitle({ children, ...restProps }) {
+	return <SubTitle {...restProps}>{children}</SubTitle>;
+};
+
+Card.Text = function CardText({ children, ...restProps }) {
+	return <Text {...restProps}>{children}</Text>;
+};
+
+Card.Meta = function CardMeta({ children, ...restProps }) {
+	return <Meta {...restProps}>{children}</Meta>;
+};
+
+Card.Entities = function CardEntities({ children, ...restProps }) {
+	return <Entities {...restProps}>{children}</Entities>;
+};
+
+Card.Item = function CardItem({ item, children, ...restProps }) {
+	const [setShowFeature, setItemFeature] = useContext(FeatureContext);
+	return (
+		<Item
+			onClick={() => {
+				setShowFeature(item);
+				setItemFeature(true);
+			}}
+			{...restProps}
+		>
+			{children}
+		</Item>
+	);
+};
+
+Card.Image = function CardImage({ ...restProps }) {
+	return <Image {...restProps} />;
+};
