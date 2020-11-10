f3525feb05fa9d97d73d59ce73aa0680de66ce78
mostafa ragab <49085627+Mostafa-Ragab@users.noreply.github.com>
2020-11-10 17:30:11 +0200

    adding item & feature

--- src/components/card/index.js
+++ src/components/card/index.js
@@ -47,21 +47,20 @@ Card.Text = function CardText({ children, ...restProps }) {
 	return <Text {...restProps}>{children}</Text>;
 };
 
+Card.Entities = function CardEntities({ children, ...restProps }) {
+	return <Entities {...restProps}>{children}</Entities>;
+};
 Card.Meta = function CardMeta({ children, ...restProps }) {
 	return <Meta {...restProps}>{children}</Meta>;
 };
 
-Card.Entities = function CardEntities({ children, ...restProps }) {
-	return <Entities {...restProps}>{children}</Entities>;
-};
-
 Card.Item = function CardItem({ item, children, ...restProps }) {
-	const [setShowFeature, setItemFeature] = useContext(FeatureContext);
+	const { setShowFeature, setItemFeature } = useContext(FeatureContext);
 	return (
 		<Item
 			onClick={() => {
-				setShowFeature(item);
-				setItemFeature(true);
+				setItemFeature(item);
+				setShowFeature(true);
 			}}
 			{...restProps}
 		>
@@ -72,4 +71,37 @@ Card.Item = function CardItem({ item, children, ...restProps }) {
 
 Card.Image = function CardImage({ ...restProps }) {
 	return <Image {...restProps} />;
+};
+
+Card.Feature = function CardFeature({ children, category, ...restProps }) {
+	const { showFeature, itemFeature, setShowFeature } = useContext(
+		FeatureContext
+	);
+
+	return showFeature ? (
+		<Feature
+			{...restProps}
+			src={`/images/${category}/${itemFeature.genre}/${itemFeature.slug}/large.jpg`}
+		>
+			<Content>
+				<FeatureTitle>{itemFeature.title}</FeatureTitle>
+				<FeatureText>{itemFeature.description}</FeatureText>
+				<FeatureClose onClick={() => setShowFeature(false)}>
+					<img src="/images/icons/close.png" alt="Close" />
+				</FeatureClose>
+
+				<Group margin="30px 0" flexDirection="row" alignItems="center">
+					<Maturity rating={itemFeature.maturity}>
+						{itemFeature.maturity < 12 ? "PG" : itemFeature.maturity}
+					</Maturity>
+					<FeatureText fontWeight="bold">
+						{itemFeature.genre.charAt(0).toUpperCase() +
+							itemFeature.genre.slice(1)}
+					</FeatureText>
+				</Group>
+
+				{children}
+			</Content>
+		</Feature>
+	) : null;
 };
