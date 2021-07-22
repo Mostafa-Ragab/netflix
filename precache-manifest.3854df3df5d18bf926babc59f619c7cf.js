83c2ccf21c148fd25501d6728af9a662af6ab862
mostafa ragab <49085627+Mostafa-Ragab@users.noreply.github.com>
2021-07-22 16:51:57 +0200

    Updates

---
+++ precache-manifest.3854df3df5d18bf926babc59f619c7cf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfa461c1500a4d9bae2ccd18d1e50f7d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "00fe3f56ad534a756af5",
+    "url": "/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "00fe3f56ad534a756af5",
+    "url": "/static/js/2.b62a7325.chunk.js"
+  },
+  {
+    "revision": "70c1250a949326ab701ae38c8bc3a317",
+    "url": "/static/js/2.b62a7325.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8919b253e1f9309d6d75",
+    "url": "/static/js/main.9d65dbfb.chunk.js"
+  },
+  {
+    "revision": "aeea36bdadbfd99c7e94",
+    "url": "/static/js/runtime-main.f7969a42.js"
+  },
+  {
+    "revision": "6dbba458959d4ce1edd2f5b3ab3ae13b",
+    "url": "/static/media/logo.6dbba458.svg"
+  }
+]);
